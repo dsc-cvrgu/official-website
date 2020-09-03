8ff1882bdffb94aca367d6e1687255290e9e2896
snehasish-20 <63896969+snehasish-20@users.noreply.github.com>
2020-09-03 12:33:50 +0530

    Add files via upload

---
+++ Snehasish/Sign-in dsc/script.js
@@ -0,0 +1,10 @@
+function register()
+{
+    document.getElementById("sign-in").style.display="none";
+    document.getElementById("sign-up").style.display="flex";
+}
+function login()
+{
+    document.getElementById("sign-up").style.display="none";
+    document.getElementById("sign-in").style.display="flex";
+}

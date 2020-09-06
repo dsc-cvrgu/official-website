2e9cc25eb6129fffa2220fc7db5deb92df4d95e0
DEEPAK KUMAR SAHOO <56642683+kumardeepak123@users.noreply.github.com>
2020-09-06 17:10:24 +0530

    team page completed in react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./components/home";
 import Events from "./components/events";
-import Team from "./components/team";
+import Team from "./components/team/team";
 import Contact from "./components/contact/contact";
 import Login from "./components/login";
 
--- frontend/src/components/team.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-class Team extends React.Component {
-  render() {
-    return <h2>Team</h2>;
-  }
-}
-export default Team;

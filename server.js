d97149128cd3c9460aa0098d54b4fecc4c981c8d
atmanad <atmanad0001@gmail.com>
2021-01-04 13:24:30 +0530

    heroku deploy

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ let port = process.env.PORT || 5000;
 const app = express();
 app.use(cors({ origin: true }));
 
-// if (process.env.NODE_ENV === "production") {
-app.use(express.static("client/build"));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => console.log("server running on port: ", port));
